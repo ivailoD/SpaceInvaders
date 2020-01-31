12b49d3b4c84aa8b4fbad52b765bda89bd048b36
ivailoD <ivaylo.dimitrov@egt-interactive.local>
2020-01-31 19:45:44 +0200

    Feat added basic functionality

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log(1323);
+//import * as PIXI from "pixi.js";
+var PIXI = require('pixi.js'); 
+function main() {
+    window.onload = function () {
+        var app = new PIXI.Application({
+            width: 800,
+            height: 600,
+            backgroundColor: 0xaaaaaa
+        });
+        document.body.appendChild(app.view);
+        console.log("app started");
+    };
+}
